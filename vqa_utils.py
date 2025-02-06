eed4f9f00c1a49da36c111a5e756627f5be0961d
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-06 00:05:53 -0800

    bug fix

--- vqa_utils.py
+++ vqa_utils.py
@@ -10,9 +10,10 @@ from scipy.ndimage import label as label_, binary_dilation, generate_binary_stru
 from collections import Counter, defaultdict
 
 
-def generate_train_val_test_splits(all_vqa_questions, question_key="seg_id", seed=0, train_seg_ids=(), val_seg_ids=(), test_seg_ids=(),
-                                   train_frac=0.8, val_frac=0.1, train_file="brats_gli_vqa_train.json",
-                                   val_file="brats_gli_vqa_val.json", test_file="brats_gli_vqa_test.json"):
+def generate_train_val_test_splits(all_vqa_questions, question_key="seg_id", seed=0, train_seg_ids=None,
+                                   val_seg_ids=None, test_seg_ids=None, train_frac=0.8, val_frac=0.1,
+                                   train_file="brats_gli_vqa_train.json", val_file="brats_gli_vqa_val.json",
+                                   test_file="brats_gli_vqa_test.json"):
     if (train_seg_ids is not None) and (val_seg_ids is not None) and (test_seg_ids is not None):
         train_questions = [q for q in all_vqa_questions if q[question_key] in train_seg_ids]
         val_questions = [q for q in all_vqa_questions if q[question_key] in val_seg_ids]
