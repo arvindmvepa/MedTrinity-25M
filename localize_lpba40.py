3176d1dd8a4922f77df378d6321425ec309d30a3
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-04-10 00:30:21 -0700

    debug

--- localize_lpba40.py
+++ localize_lpba40.py
@@ -21,12 +21,13 @@ def load_labels(label_txt_path):
 def compute_overlap(tumour_img, atlas_img):
     tumour_data = tumour_img.get_fdata() > 0
     atlas_data = atlas_img.get_fdata().astype(int)[..., 0]  # Assuming the atlas is a single channel
-    print(f"Atlas shape: {atlas_data.shape}, Tumour shape: {tumour_data.shape}")
-    print(f"atlas_data min: {np.min(atlas_data)}, max: {np.max(atlas_data)}")
     assert atlas_data.shape == tumour_data.shape, "Atlas and tumour images must have the same shape."
 
     # Only look at tumour voxels
     overlapped_labels = atlas_data[tumour_data]
+
+    print(f"overlapping labels: {overlapped_labels.shape}")
+    print(f"(non-zero) overlapping labels: {np.sum(overlapped_labels > 0)}")
 
     unique, counts = np.unique(overlapped_labels[overlapped_labels > 0], return_counts=True)
     total_voxels = np.sum(tumour_data)
