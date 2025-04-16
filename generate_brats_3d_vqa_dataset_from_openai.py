70278a400e369e26dc5f74e1cc5fa14801c094a1
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-04-16 01:35:16 -0700

    generate openai dataset for updated dataset

--- generate_brats_3d_vqa_dataset_from_openai.py
+++ generate_brats_3d_vqa_dataset_from_openai.py
@@ -290,7 +290,7 @@ if __name__ == "__main__":
 
     # GLI dataset settings
     dataset_type = "gli"
-    version = f"updated_v1_seed{dataset_seed}"
+    version = f"updated_v2_seed{dataset_seed}"
     labels_order = (1, 2, 3, 4)
     pediatric = False
     goat = False
