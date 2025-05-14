c7ac5c4d44d81458040ebf62da1eeefe1922fc79
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-05-13 20:26:13 -0700

    get updated openai dataset

--- create_brats_3d_vqa_dataset_from_openai.py
+++ create_brats_3d_vqa_dataset_from_openai.py
@@ -410,9 +410,16 @@ if __name__ == "__main__":
     new_dataset_seed = 0
 
     # GLI dataset settings
-    dataset_type = "gli"
-    version = f"updated_v10_seed{dataset_seed}"
-    labels_order = (1, 2, 3, 4)
+    #dataset_type = "gli"
+    #version = f"updated_v2_seed{dataset_seed}"
+    #labels_order = (1, 2, 3, 4)
+    #pediatric = False
+    #goat = False
+
+    # MET dataset settings
+    dataset_type = "met"
+    version = f"updated_v3_seed{dataset_seed}"
+    labels_order = (1, 2, 3)
     pediatric = False
     goat = False
 
@@ -425,7 +432,7 @@ if __name__ == "__main__":
 
     # GoAT dataset settings
     #dataset_type = "goat"
-    #version = f"updated_v2_seed{dataset_seed}"
+    #version = f"updated_v3_seed{dataset_seed}"
     #labels_order = (1, 2, 3)
     #pediatric = False
     #goat = True
