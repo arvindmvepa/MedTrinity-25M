c8f8b14ae55494099a0df7c06705b38ab785ea1a
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-06-02 16:50:02 -0700

    bug fix

--- create_nlst_3d_aux_dataset.py
+++ create_nlst_3d_aux_dataset.py
@@ -77,10 +77,7 @@ def convert_dict_to_numeric(original_data):
             "longest_diameter": longest_diameter,
             "longest_perpendicular_diameter": longest_perpendicular_diameter,
         }
-        print(img_files)
-        print(init_study_yr)
-        print(final_study_yr)
-        new_data[[(tuple(img_files), init_study_yr, final_study_yr)]] = new_content_type_dict
+        new_data[(tuple(img_files), init_study_yr, final_study_yr)] = new_content_type_dict
 
     return new_data
 
