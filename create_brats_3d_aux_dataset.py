6f37174c6d8e369f041d8c293a7f5c7e2a794372
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-11-19 15:22:56 -0800

    bug fix

--- create_brats_3d_aux_dataset.py
+++ create_brats_3d_aux_dataset.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
 
     # MET dataset settings
     dataset_type = "met"
-    version = "v11_seed{seed}"
+    version = f"v11_seed{seed}"
     volume_file_dirs = sorted(list(glob(f'/local2/shared_data/BraTS2024-BraTS-MET/MICCAI-BraTS2024-MET-Challenge-Training_overall/*')))
     labels_order = (1, 2, 3)
     pediatric = False
