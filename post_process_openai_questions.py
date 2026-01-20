2c76fecee0c2898cde2f9b35e252775c11fbefb5
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-01-20 03:02:52 -0800

    bug fix

--- post_process_openai_questions.py
+++ post_process_openai_questions.py
@@ -76,8 +76,8 @@ if __name__ == "__main__":
     #input_csv = "mri_dataset_draft_v1_combined.csv"
     #input_csv = "mri_dataset_partially_unknown_combined1.csv"
     input_csv = "mri_dataset_unknown.csv"
-    # output_csv = "mri_dataset_draft_clean_v1.csv"
-    #output_csv = "mri_dataset_partially_unknown_combined1_clean_v1.csv"
+    # output_csv = "mri_dataset_draft_v1_combined_clean_v1.csv"
+    # output_csv = "mri_dataset_partially_unknown_combined1_clean_v1.csv"
     output_csv = "mri_dataset_unknown_clean_v1.csv"
     df = generate_clean_qa_dataset_from_openai(input_csv)
     # mask out the chinese
