349507fd50370101f1ea51c2e19b7fa9426605e9
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-02-03 14:58:41 -0800

    bug fix

--- kappa_agreement_analysis.py
+++ kappa_agreement_analysis.py
@@ -127,12 +127,7 @@ def create_case_mapping(prediction_data):
     case_map = {}
     
     for i, pred in enumerate(prediction_data):
-        if 'seg_file' in pred:
-            case_name = extract_case_name(pred['seg_file'])
-        elif 'mpMRI' in pred:
-            case_name = pred['mpMRI']
-        else:
-            raise ValueError(f"Prediction data item {i} missing 'seg_file' or 'mpMRI' key")
+        case_name = extract_case_name(pred['seg_file'])
         case_map[case_name] = pred
     
     return case_map
