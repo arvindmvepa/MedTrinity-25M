8fdabc4bda73e4afd2ce3518aa11152549cfb9bd
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-02-11 16:40:29 -0800

    add progress bar

--- create_nlst_3d_dataset.py
+++ create_nlst_3d_dataset.py
@@ -439,7 +439,7 @@ def generate_vqa_from_df(ann_df, add_time_delta2=False, save_dir="/hsuraid/avepa
     """
     all_vqas = []
     question_index = 0
-    for pid, pid_ann_df in ann_df.groupby('pid'):
+    for pid, pid_ann_df in tqdm(ann_df.groupby('pid')):
         inst = pid_ann_df['cen'].iloc[0]
 
         pid_study_yr0_ann_df = pid_ann_df.loc[pid_ann_df["study_yr"] == 0]
