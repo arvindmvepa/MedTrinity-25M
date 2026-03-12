f4a894f2675f112bf92bd9738f1d2946529d89f3
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-03-11 22:22:29 -0700

    parallelize the copying script

--- copy_nlst_3d_dataset.py
+++ copy_nlst_3d_dataset.py
@@ -2,6 +2,8 @@ import pandas as pd
 from glob import glob
 import os
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
 
 df = pd.read_csv("pid2split.csv")
 pids = df['PID'].tolist()
@@ -11,18 +13,25 @@ save_dir = "/hsuraid/avepa/nlst_npy_m3fm"
 
 os.makedirs(save_dir, exist_ok=True)
 
-for pid in tqdm(pids):
+def process_pid(pid):
     patient_dir = os.path.join(root_npy_dir, str(pid))
     if not os.path.exists(patient_dir):
-        continue
+        return
 
     time_points = sorted(glob(os.path.join(patient_dir, "*")))
     for time_index, time_point in enumerate(time_points):
         img_npy_files = sorted(glob(os.path.join(time_point, "*")))
         if not img_npy_files:
             continue
+
         src = img_npy_files[0]
         dst = os.path.join(save_dir, f"pid{pid}_ts{time_index}.npy")
 
-        if not os.path.exists(dst):
-            os.symlink(src, dst)
+        if not os.path.lexists(dst):
+            try:
+                os.symlink(src, dst)
+            except FileExistsError:
+                pass
+
+with ThreadPoolExecutor(max_workers=16) as ex:
+    list(tqdm(ex.map(process_pid, pids), total=len(pids)))
