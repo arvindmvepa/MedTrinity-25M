6d11f882d9621b17fe2e405ab1e8fea4cd4d5b9a
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-04-07 19:59:24 -0700

    bug fix

--- create_report_dataset.py
+++ create_report_dataset.py
@@ -31,19 +31,30 @@ if __name__ == "__main__":
         test_data = json.load(f)
     
     train_reports = []
+    previously_seen = set()
     for item in train_data:
         seg_id = os.path.basename(item["volume_file_dir"])
-        if seg_id in report_data:
+        if seg_id in report_data and seg_id not in previously_seen:
+            item["question"] = "What is the impression of the radiology report for this MRI scan?"
             item["answer_gen"] = report_data[seg_id]
-            train_reports.append(item["answer_gen"])    
+            train_reports.append(item["answer_gen"])
+            previously_seen.add(seg_id)
+    val_reports = []
     for item in val_data:
         seg_id = os.path.basename(item["volume_file_dir"])
-        if seg_id in report_data:
+        if seg_id in report_data and seg_id not in previously_seen:
+            item["question"] = "What is the impression of the radiology report for this MRI scan?"
             item["answer_gen"] = report_data[seg_id]
+            val_reports.append(item["answer_gen"])
+            previously_seen.add(seg_id)
+    test_reports = []
     for item in test_data:
         seg_id = os.path.basename(item["volume_file_dir"])
-        if seg_id in report_data:
+        if seg_id in report_data and seg_id not in previously_seen:
+            item["question"] = "What is the impression of the radiology report for this MRI scan?"
             item["answer_gen"] = report_data[seg_id]
+            test_reports.append(item["answer_gen"])
+            previously_seen.add(seg_id) 
     print(f"Number of training samples with reports: {len(train_reports)}")
-    print(f"Number of validation samples with reports: {len(val_data)}")
-    print(f"Number of test samples with reports: {len(test_data)}") 
+    print(f"Number of validation samples with reports: {len(val_reports)}")
+    print(f"Number of test samples with reports: {len(test_reports)}") 
